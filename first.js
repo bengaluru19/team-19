9d7718d3c78e1468738cc4ecab7b69a984f14069
NirvanaDogra <32649087+NirvanaDogra@users.noreply.github.com>
2019-07-06 18:01:18 +0530

    Commit -Nirvana

---
+++ first.js
@@ -0,0 +1,52 @@
+var express= require('express');
+var app=express();
+var MongoClient = require('mongodb').MongoClient
+var admin = require("firebase-admin");
+
+
+var serviceAccount = require("./google.json");
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://team19-8586d.firebaseio.com"
+});
+
+var db = admin.firestore();
+
+
+// let cityRef = db.collection('team19_item').doc('BJ');
+// let data = {
+//   name: 'Los Angeles',
+//   state: 'CA',
+//   country: 'USA'
+// };
+
+// // Add a new document in collection "cities" with ID 'LA'
+// let setDoc = db.collection('team19_item').doc('LA').set(data);
+
+
+// let doc = db.collection('team19_item').doc('LA');
+
+// let observer = doc.onSnapshot(docSnapshot => {
+//   console.log(`Received doc snapshot: ${docSnapshot}`);
+//   doc.data()
+// }, err => {
+//   console.log(`Encountered error: ${err}`);
+// });
+
+query = db.collection('team19_item');
+
+let observer = query.onSnapshot(querySnapshot => {
+  querySnapshot.docChanges().forEach(change => {
+    if (change.type === 'added') {
+      console.log('New city: ', change.doc.data());
+    }
+    if (change.type === 'modified') {
+      console.log('Modified city: ', change.doc.data());
+    }
+    if (change.type === 'removed') {
+      console.log('Removed city: ', change.doc.data());
+    }
+  });
+});
